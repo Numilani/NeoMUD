5ab11113bcb79c7f115fb3dc2f4a2068e27f7c88
numilani <numilani@protonmail.com>
2025-06-02 00:00:57 -0400

    add base splash screen and wraper logic

---
+++ src/NeoMUD.cs
@@ -0,0 +1,80 @@
+﻿using Serilog;
+
+namespace NeoMUD;
+
+public class NeoMUD
+{
+  public static void Main()
+  {
+    List<string> RunningThreadList = new();
+    DateTime startupTime = DateTime.Now;
+    CancellationTokenSource cts = new();
+
+    // initialize background services
+    Log.Logger = new LoggerConfiguration()
+      .MinimumLevel.Debug()
+      .WriteTo.Console()
+      .WriteTo.File("logs/debug.log", rollingInterval: RollingInterval.Hour)
+      .WriteTo.SQLite("data.db", storeTimestampInUtc: true)
+      .CreateLogger();
+
+    DisplayStartupSplash(startupTime);
+
+    // Spin up background threads
+    Thread serverTickHandler = new(() => TickServer(cts.Token, ref RunningThreadList));
+
+    serverTickHandler.Start();
+
+    // Add handler for graceful shutdown on Ctrl-C
+    Console.CancelKeyPress += (object? sender, ConsoleCancelEventArgs e) =>
+    {
+      e.Cancel = true;
+      InitiateShutdown(cts, ref RunningThreadList);
+    };
+  }
+
+  public static void DisplayStartupSplash(DateTime startTime)
+  {
+    String startupTimeString21 = startTime.ToString("yyyy-MM-dd HH:mm:ss  ");
+    Console.WriteLine($"""
+===============================================================================
+                             NeoMUD 0.0.1 Alpha
+Starting At:                                                  Stop server with:
+{startupTimeString21}                                                   CTRL-C
+===============================================================================
+""");
+  }
+
+  public static void InitiateShutdown(CancellationTokenSource cts,
+      ref List<string> runningThreadList)
+  {
+    cts.Cancel();
+    while (runningThreadList.Count > 0)
+    {
+      // do nothing
+    }
+    String stoppedTimeString21 = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss  ");
+    Console.WriteLine($"""
+===============================================================================
+                            NeoMUD shutting down
+Stopping At:                                                 Graceful Shutdown:
+{stoppedTimeString21}                                               SUCCESSFUL
+===============================================================================
+""");
+  }
+
+  public static void TickServer(CancellationToken ct, ref List<string> runningThreadList)
+  {
+    runningThreadList.Add("TickServer");
+    Log.Information("Started server tick thread");
+    while (!ct.IsCancellationRequested)
+    {
+      // do server ticking logic here
+    }
+
+    // following code runs after cancellation requested
+    Log.Information("Stopping server tick thread...");
+    runningThreadList.Remove("TickServer");
+  }
+
+}
