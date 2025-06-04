c03592436d156a8ff7327dad35f7eee72352873c
numilani <numilani@protonmail.com>
2025-06-03 23:57:16 -0400

    testing ideas

--- src/Interactable.cs
+++ src/Interactable.cs
@@ -1,3 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using shortid;
+using shortid.Configuration;
+
+[Table("Interactables")]
 public class Interactable{
 
+  [Key]
+  public string Id {get; set;} = ShortId.Generate(new GenerationOptions(useSpecialCharacters: false));
 }
+
