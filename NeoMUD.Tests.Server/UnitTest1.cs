3b49e606f690f16ad17205b74f0392880943a5d7
numilani <numilani@protonmail.com>
2025-08-02 23:47:33 -0400

    wip reorganization

---
+++ NeoMUD.Tests.Server/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace NeoMUD.Tests.Server;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
